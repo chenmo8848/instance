e14530bd79ce9336b400756d8ef0da966d1bdbf9
蜘蛛子QAQ <88091853+ZhizhuziQAQ@users.noreply.github.com>
2021-07-30 17:59:41 +0800

    Update instance.sh
    修复错别字

--- instance.sh
+++ instance.sh
@@ -197,7 +197,7 @@ while [[ true ]]; do
     409)
 		echo -e "["$current_time"]" "实例状态：${Font_Red}Apply conflict${Font_Suffix}, 返回状态：""${Font_Red}${outcome}${Font_Suffix}"  
 		echo -e "["$current_time"]" "实例状态：${Font_Red}Apply conflict${Font_Suffix}, 返回状态：""${Font_Red}${outcome}${Font_Suffix}" >> /root/oci_error.log
-    	Msg_error="【甲骨文信息】：${Instance_Name}申请脚本已停止，返回信息为Apply conflict"
+    	        Msg_error="【甲骨文信息】：${Instance_Name}申请脚本已停止，返回信息为Apply conflict"
 		curl -s -X POST $URL -d chat_id=${CHAT_ID} -d text="${Msg_error}"
 		break
     ;;
