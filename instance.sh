d2cc089059fc21f4592a575ce1c91dd9f20220d6
蜘蛛子QAQ <88091853+ZhizhuziQAQ@users.noreply.github.com>
2021-07-30 09:36:24 +0800

    Update instance.sh

--- instance.sh
+++ instance.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #==========================================================
-#	Description: 甲骨文ARM自动申请脚本					   
+#	Description: 甲骨文ARM自动申请脚本（使用教程在博客）					   
 #	Version: 3.2 					  
 #	Author: 蜘蛛子
-#       Blog：https://www.zhizhuzi.org      使用教程在博客
+#       Blog：https://www.zhizhuzi.org      
 # 	Telegram: https://t.me/Zhizhuzi			   
 #	Github: https://github.com/ZhizhuziQAQ/instance	   
 #	Latest Update: Fri 30 Jul 2021 09:20:55 AM CST    
